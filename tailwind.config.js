f30f5e0df6baa64dde29282287be8e8a62d209e3
Shivam Veerabudren <shivamveerabudren@gmail.com>
2022-11-28 11:32:02 -0500

    ...

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,7 @@ module.exports = {
         'lg': '1024px',
         // => @media (min-width: 1024px) { ... }
   
-        'xl': '2600px',
+        'xl': {'min': '1280px', 'max': '1635px'},
         // => @media (min-width: 1280px) { ... }
   
         '2xl': '1536px',
