b8320ddb66c597d0bd360c84f051cdd0cadf19be
Shivam Veerabudren <shivamveerabudren@gmail.com>
2022-11-29 15:53:49 -0500

    Added release date to modal card

--- src/Components/ModalCard.jsx
+++ src/Components/ModalCard.jsx
@@ -5,6 +5,7 @@ import conditionalClasses from "../util/conditionalClasses";
 const ModalCard = (props) => {
   const genre = useContext(GenreContext);
   const [genreList, setGenreList] = useState([]);
+  const releaseDate = new Date(props.obj.release_date);
 
   useEffect(() => {
     let resList = [];
@@ -43,6 +44,7 @@ const ModalCard = (props) => {
             </div>
           );
         })}
+        <p>Release Date: <span>{releaseDate.getDate()} {monthToString(releaseDate.getMonth())} {releaseDate.getFullYear()}</span></p>
         </div>
       </div>
     </div>
@@ -50,3 +52,20 @@ const ModalCard = (props) => {
 };
 
 export default ModalCard;
+
+const monthToString = (monthNumber) => {
+    switch (monthNumber){
+      case 0: return "Jan"; break;
+      case 1: return "Feb"; break;
+      case 2: return "Mar"; break;
+      case 3: return "Apr"; break;
+      case 4: return "May"; break;
+      case 5: return "Jun"; break;
+      case 6: return "Jul"; break;
+      case 7: return "Aug"; break;
+      case 8: return "Sep"; break;
+      case 9: return "Oct"; break;
+      case 10: return "Nov"; break;
+      case 11: return "Dec"; break;
+    }
+}
